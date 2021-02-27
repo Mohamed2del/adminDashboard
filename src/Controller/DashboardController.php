a7f0a2c7edf11212ba4fa6deeffa6a04505eef00
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-02-27 23:35:38 +0200

    SQL SOLVE

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -14,13 +14,46 @@ class DashboardController extends AbstractController
 
     public function number(): Response
     {
-        $userFirstName = "Mohamed";
-        $notifications = ['...','....'];
+       
+      
 
+    
         return $this->render('/home.html.twig', [
-
-            'user_first_name' => $userFirstName ,
-            'notifications' => $notifications,
-        ]);
+                    
+                    ]);
     }
+
+         /**
+      * @Route("/test")
+      */
+
+      public function z(): Response
+      {
+         
+          $connectionParams = array(
+            'url' => "postgresql://user_resoneo__readonly2:sfx_yhbbuAHJXhH5eee45DHJEa@51.83.37.209:5433/sekoya?serverVersion=13&charset=utf8",
+        );
+        $sql = "SELECT job_status, count(*)
+        FROM queue.jobs 
+        WHERE 1=1
+        -- filter about date :             AND job_added_at::date = <date>
+        -- filter about descriptor :   AND job_descriptor::date ILIKE <free_search_string>
+        GROUP BY job_status ";
+
+       
+
+        $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+
+        $stmt = $conn->query($sql); // Simple, but has several drawbacks
+
+        $arr = array();
+        while (($row = $stmt->fetchAssociative()) !== false) {
+          array_push($arr,$row['job_status'] ) ;
+      }
+
+          return $this->render('/pog.html.twig', [
+                      'conn' => $arr
+                      ]);
+      }
 }
+
